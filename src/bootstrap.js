7d78990b6b594d3ed1dc898ea1aebf9b541ca527
goze <goze.qiu@gmail.com>
2017-07-03 23:32:52 +0800

    refactor: user map && editor

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,36 +2,24 @@
 import './theme/theme.scss';
 
 // classes you want to use immediately
-import {App} from './App';
-import {Http} from './Http';
+import {GMap} from './Map';
+import {GEditor} from './Editor';
+import $ from 'jquery';
 
 /**
  * entrance code for SPA
  */
 function main() {
-  document.title = 'Loading...';
-
-  const app = new App({
-    dom: document.querySelector('.container'),
-    prop1: 'prop1',
-    prop2: 'prop2',
-    prop3: 'prop3'
+  $('.container').css({
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    'flex-direction': 'row'
   });
-
-  // we can make requests to multiple domains, check out proxy/rules.js
-  const http = new Http();
-
-  // send request to github.com
-  http.get('/node-0/search/repositories?o=desc&q=es6&s=stars&type=Repositories&utf8=✓').then((res) => {
-    const data = JSON.parse(res);
-    app.render(data);
-    document.title = 'App Started';
-  });
-
-  // send request to npmjs.org
-  http.get('/node-1').then((json) => console.log('From npmjs.org: ', json));
-
-  console.log([1, 2, 3, 4].includes(1)); // ES7: Array.prototype.includes
+  window.Map = new GMap();
+  window.Editor = new GEditor();
+  window.Editor.init();
+  Map.setCenter(123, 23, 1);
 }
 
 document.addEventListener('DOMContentLoaded', main);
