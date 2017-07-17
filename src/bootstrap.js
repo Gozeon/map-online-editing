2c457db99adc2c8530f9a77fe39a899b7614c85c
goze <goze.qiu@gmail.com>
2017-07-17 11:14:24 +0800

    feat: add hint worker && console

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -14,9 +14,17 @@ import * as style from './main.scss';
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHozMTY0MjQiLCJhIjoiNzI3NmNkOTcyNWFlNGQxNzU2OTA1N2EzN2FkNWIwMTcifQ.NS8KWg47FzfLPlKY0JMNiQ';
 
 $('div.container').addClass(style.container);
+$('div.container').append('<div id="top"></div>');
+$('div#top').addClass(style.top);
+
+// console
+$('div#top').append('<div id="console"></div>');
+$('div#console').addClass(style.console);
+$('div#console').append('<h3>Metrics</h3>');
+$('div#console').append('<ul id="errorsList"></ul>');
 
 // editor
-$('div.container').append('<div id="editor"></div>');
+$('div#top').append('<div id="editor"></div>');
 $('div#editor').addClass(style.editor);
 const editor = ace.edit('editor');
 const defaultValue = [
@@ -37,10 +45,34 @@ editor.commands.addCommand({
   name: 'runCommond',
   bindKey: {win: 'Ctrl+Enter', mac: 'Command-Enter'},
   exec: (editor) => {
+    if ($('#scripts').get(0)) {
+      $('#scripts').remove();
+    }
+    if ($('ul#errorsList').find('li').length > 0) {
+      return;
+    }
     $('body').append(`<script id="scripts">${editor.getValue()}</script>`);
   },
   readOnly: true
 });
+const Worker = require('worker-loader!./worker.js');
+const worker = new Worker; // eslint-disable-line
+editor.getSession().on('change', function() {
+  worker.postMessage(editor.getValue());
+});
+worker.onmessage = function (e) {
+  $('ul#errorsList').empty();
+  if (Object.prototype.hasOwnProperty.call(e.data, 'errors')) {
+    for (let i = 0; i < e.data.errors.length; i++) {
+      $('ul#errorsList').append(`<li>${e.data.errors[i].reason}</li>`);
+    }
+  }
+  if (Object.prototype.hasOwnProperty.call(e.data, 'unused')) {
+    for (let i = 0; i < e.data.unused.length; i++) {
+      $('ul#errorsList').append(`<li>unused: ${e.data.unused[i].name}</li>`);
+    }
+  }
+};
 
 // map
 $('div.container').append('<div id="map"></div>');
